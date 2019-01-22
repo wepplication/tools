28ae612e7f7f6a52e853d5077871ea8bb7a3ef8a
weppl <imagine87@hotmail.com>
2019-01-22 05:20:33 +0000

    add crypt tools

--- js/browserify/eml-format.bundle.js
+++ js/browserify/eml-format.bundle.js
@@ -1,6 +1,9 @@
 /******************************************************************************************
  * eml-format 0.5.0
+ * https://github.com/papnkukn/eml-format
  * browserify -r ./eml-format.js:eml-format  > eml-format.bundle.js
+ * var emlformat = require('eml-format');
+ * emlformat.read(string_or_object_eml, function(error, data) {});
  ******************************************************************************************/
 require=(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 'use strict'
--- js/browserify/subsrt.bundle.js
+++ js/browserify/subsrt.bundle.js
@@ -1,4 +1,10 @@
-// browserify -r ./subsrt.js:subsrt -r ./format/vtt.js -r ./format/lrc.js -r ./format/smi.js -r ./format/ssa.js -r ./format/ass.js -r ./format/sub.js -r ./format/srt.js -r ./format/sbv.js -r ./format/json.js > subsrtbundle.js
+/******************************************************************************************
+ * subsrt 1.0.0
+ * https://github.com/papnkukn/subsrt
+ * browserify -r ./subsrt.js:subsrt -r ./format/vtt.js -r ./format/lrc.js -r ./format/smi.js -r ./format/ssa.js -r ./format/ass.js -r ./format/sub.js -r ./format/srt.js -r ./format/sbv.js -r ./format/json.js > subsrtbundle.js
+ * var subsrt = require("subsrt");
+ * subsrt.convert(sub, { format: 'srt' });
+ ******************************************************************************************/
 require=(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({"/format/ass.js":[function(require,module,exports){
 'use strict';
 
